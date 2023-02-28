bee7866e9f58f1a669ac6e91661ade7815f9600e
dragonboyy <elliot.verstraelen@telenet.be>
2023-02-28 11:41:22 +0100

    Implementation of classes Symbol and Lexer

--- src/main/java/compiler/Lexer/Lexer.java
+++ src/main/java/compiler/Lexer/Lexer.java
@@ -2,11 +2,20 @@ package compiler.Lexer;
 import java.io.Reader;
 
 public class Lexer {
+
+    private Reader sourceFile;
+    private Symbol currentSymbol
     
     public Lexer(Reader input) {
+        this.sourceFile = sourceFile;
+        this.currentSymbol = NULL // Is this correct instanciation ?
+    }
+    private int readNextChar() throws IOException {
+        return this.currentChar = this.sourceFile.read();
     }
     
-    public Symbol getNextSymbol() {
-        return null;
+    public Symbol getNextSymbol() throws IOException {
+        //Implement the logic
     }
+
 }
--- src/main/java/compiler/Lexer/Symbol.java
+++ src/main/java/compiler/Lexer/Symbol.java
@@ -1,4 +1,18 @@
 package compiler.Lexer;
 
 public class Symbol {
+    private TokenType tokenType;
+    private String lexem;
+
+    public Symbol(TokenType tokenType, String lexeme) {
+        this.tokenType = tokenType;
+        this.lexeme = lexeme;
+    }
+
+    public tokenType getTokenType(){
+        return this.tokenType;
+    }
+    public String getLexeme() {
+        return this.lexeme;
+    }
 }
