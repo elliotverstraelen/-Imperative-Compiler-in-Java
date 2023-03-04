db0fb32498bceb3278bef8205179ae8f9a0e09ea
Dylan Goffinet <goffindy@gmail.com>
2023-03-04 16:43:49 +0100

    Extra comments for tests

--- test/TestLexer.java
+++ test/TestLexer.java
@@ -9,6 +9,9 @@ import static org.junit.Assert.*;
 
 public class TestLexer {
 
+    /**
+     * Test if the lexer does not return null for the first symbol on a correct input
+     */
     @Test
     public void testNotNull() {
         String input = "var x int = 2;";
@@ -17,6 +20,9 @@ public class TestLexer {
         assertNotNull(lexer.getNextSymbol());
     }
 
+    /**
+     * Test if the lexer ignores comments
+     */
     @Test
     public void testComments() {
         String input = "//This is a comment\nvar x int = 2;";
@@ -25,6 +31,9 @@ public class TestLexer {
         assertEquals(lexer.currentSymbol.getLexeme(), "var");
     }
 
+    /**
+     * Test if the lexer correctly identifies the symbols in the input
+     */
     @Test
     public void testSymbols() {
         String input = "var x bool = true; val y int = 2; val x real = 2.0; val z string = \"Hello World\";";
@@ -41,12 +50,16 @@ public class TestLexer {
         Lexer lexer = new Lexer(reader);
         Symbol symbol = lexer.currentSymbol;
         for (Symbol expectedSymbol : expected) {
-            assertEquals(symbol.getLexeme(), expectedSymbol.getLexeme());
+            // This will fail if the lexer returns a symbol or lexeme that is not in the expected array
             assertEquals(symbol.getSymbolType(), expectedSymbol.getSymbolType());
+            assertEquals(symbol.getLexeme(), expectedSymbol.getLexeme());
             symbol = lexer.getNextSymbol();
         }
     }
 
+    /**
+     * Simple test to check if the lexer returns the correct first symbol
+     */
     @Test
     public void simpleTest() {
         String input = "var x int = 2;";
@@ -55,6 +68,9 @@ public class TestLexer {
         assertEquals(lexer.currentSymbol.getLexeme(), "var");
     }
 
+    /*
+     * Test if the lexer correctly identifies the symbols in a more complex input (including comments and newlines)
+     */
     @Test
     public void moreComplexTest() {
         String input = "var x int = 2;//This should not appear\n var y int = ((3 + 4) * 5);";
@@ -65,6 +81,8 @@ public class TestLexer {
         assertEquals(symbol.getLexeme(), expected[0]);
         int index = 1;
         while ((symbol = lexer.getNextSymbol()).getLexeme() != null) {
+            // This will fail if the lexer returns a symbol that is not in the expected array
+            // Only stops when the lexer returns a null symbol (EOF)
             assertEquals(symbol.getLexeme(), expected[index]);
             index++;
         }
