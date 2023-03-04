6d772a2343662a7f60d260c905889c9227bc4c35
Dylan Goffinet <goffindy@gmail.com>
2023-03-04 16:36:07 +0100

    Symbol types tests

--- test/TestLexer.java
+++ test/TestLexer.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import java.io.StringReader;
 import compiler.Lexer.Lexer;
 
+import static compiler.Lexer.Lexer.SymbolType.*;
 import static org.junit.Assert.*;
 
 public class TestLexer {
@@ -17,16 +18,38 @@ public class TestLexer {
     }
 
     @Test
-    public void simpleTest() {
-        String input = "var x int = 2;";
+    public void testComments() {
+        String input = "//This is a comment\nvar x int = 2;";
         StringReader reader = new StringReader(input);
         Lexer lexer = new Lexer(reader);
         assertEquals(lexer.currentSymbol.getLexeme(), "var");
     }
 
     @Test
-    public void testComments() {
-        String input = "//This is a comment\nvar x int = 2;";
+    public void testSymbols() {
+        String input = "var x bool = true; val y int = 2; val x real = 2.0; val z string = \"Hello World\";";
+        Symbol[] expected = {new Symbol(KEYWORD_VAR, "var"), new Symbol(IDENTIFIER, "x"),
+                new Symbol(IDENTIFIER, "bool"), new Symbol(SYMBOL_ASSIGN, "="),
+                new Symbol(BOOLEAN, "true"), new Symbol(SYMBOL_SEMICOLON, ";"), new Symbol(KEYWORD_VAL, "val"),
+                new Symbol(IDENTIFIER, "y"), new Symbol(IDENTIFIER, "int"), new Symbol(SYMBOL_ASSIGN, "="),
+                new Symbol(INTEGER, "2"), new Symbol(SYMBOL_SEMICOLON, ";"), new Symbol(KEYWORD_VAL, "val"),
+                new Symbol(IDENTIFIER, "x"), new Symbol(IDENTIFIER, "real"), new Symbol(SYMBOL_ASSIGN, "="),
+                new Symbol(REAL, "2.0"), new Symbol(SYMBOL_SEMICOLON, ";"), new Symbol(KEYWORD_VAL, "val"),
+                new Symbol(IDENTIFIER, "z"), new Symbol(IDENTIFIER, "string"), new Symbol(SYMBOL_ASSIGN, "="),
+                new Symbol(STRING, "Hello World"), new Symbol(SYMBOL_SEMICOLON, ";"), new Symbol(EOF, null)};
+        StringReader reader = new StringReader(input);
+        Lexer lexer = new Lexer(reader);
+        Symbol symbol = lexer.currentSymbol;
+        for (Symbol expectedSymbol : expected) {
+            assertEquals(symbol.getLexeme(), expectedSymbol.getLexeme());
+            assertEquals(symbol.getSymbolType(), expectedSymbol.getSymbolType());
+            symbol = lexer.getNextSymbol();
+        }
+    }
+
+    @Test
+    public void simpleTest() {
+        String input = "var x int = 2;";
         StringReader reader = new StringReader(input);
         Lexer lexer = new Lexer(reader);
         assertEquals(lexer.currentSymbol.getLexeme(), "var");
